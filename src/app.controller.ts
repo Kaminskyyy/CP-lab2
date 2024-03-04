ba8d9417a2b847ed2ddd9f64a18049cb57e683d5
Stanislav Malakhov <thehustaan@gmail.com>
2024-03-04 19:14:24 +0200

    add calc

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,30 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  getIndex() {
+    return {};
+  }
+
+  @Get('/calc')
+  @Render('result')
+  getResult(
+    @Query('a_num') a: number,
+    @Query('b_num') b: number,
+    @Query('operation') operation: string,
+  ) {
+    a = Number(a);
+    b = Number(b);
+
+    let result: number | null = null;
+    if (operation === '+') result = this.appService.sum(a, b);
+    if (operation === '-') result = this.appService.diff(a, b);
+
+    return {
+      a,
+      b,
+      operation,
+      result,
+    };
   }
 }
